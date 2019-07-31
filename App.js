5c67890861c8f6cefdbbecc957e362e53c013e42
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-07-31 09:32:30 -0300

    chore: Initital

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StatusBar } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
