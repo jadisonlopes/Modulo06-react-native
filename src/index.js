cd9fdc73212755bc09169411ead4f505708de63b
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-07-31 12:22:08 -0300

    chore: Added vector-icons and Main page configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import './config/ReactotronConfig';
+import Routes from './routes';
 
 const App = () => {
   return (
-    <View>
-      <StatusBar barStyle="dark-content" />
-      <Text>Hello World</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 };
 
