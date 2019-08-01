18488a860585f316a20259adf98d6f589629d93c
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-08-01 17:34:45 -0300

    feature: Consuming api of the github

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
+
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  static = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const { data } = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({
+      stars: data,
+    });
+  }
+
+  render() {
+    const { stars } = this.state;
+
+    return <View />;
+  }
 }
